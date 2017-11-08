559f93a9a7df52cc6808388a329cbc4fb1172a56
Ian <isseiler@users.noreply.github.com>
2017-11-08 00:31:17 -0800

    Pull external IP from a safer source (#2)

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const MEMOIZE_OPTIONS = {
 }
 
 const getExternalIp = memoize(() => (
-  fetch('http://icanhazip.com').then(response => response.text())
+  fetch('https://ifconfig.co/ip').then(response => response.text())
 ), MEMOIZE_OPTIONS)
 
 const getLocalIp = memoize(() => {
